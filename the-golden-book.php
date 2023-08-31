017e0162089042ade1da68d9e6e237f5450a7db3
Nelitha Vindinu Priyawansha <nelithavindinu7@gmail.com>
2023-08-31 07:09:06 +0530

    golden

--- the-golden-book.php
+++ the-golden-book.php
@@ -58,7 +58,7 @@
                 <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s" style="min-height: 400px;">
                     <div class="position-relative h-100">
                         <img class="img-fluid position-absolute w-100 h-100"
-                            src="content/img/the-goldenbook-education-pwc.jpg" alt="" style="object-fit: cover;">
+                            src="content/img/golden-book-edu-pwc.png" alt="" style="object-fit: cover;">
                     </div>
                 </div>
                 <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.3s">
@@ -69,13 +69,14 @@
                         contributions to education. It stands proudly in the Golden Book of Excellent Cambrians in the
                         education sector. The college, with its rich history and dedication to learning, has played a
                         vital role in shaping bright minds. Its commitment to fostering knowledge and skills has made it
-                        a beacon of education. Through the years, Prince of Wales College has been a place where
+                        a beacon of education. <br><br>Through the years, Prince of Wales College has been a place where
                         students have blossomed into knowledgeable individuals, ready to contribute positively to
                         society. Its inclusion in the Golden Book is a testament to its enduring impact and the
                         countless Cambrians it has equipped for a brighter future.
                     </p>
                     <a class="btn btn-primary py-3 px-5 mt-2 wow zoomIn" href="the-golden-book/education-sector/"
                         style="border-radius=25px" data-wow-delay="0.7s">View Book</a>
+                        
                 </div>
 
 
@@ -100,7 +101,7 @@
 
                 <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s" style="min-height: 400px;">
                     <div class="position-relative h-100">
-                        <img class="img-fluid position-absolute w-100 h-100" src="content/img/" alt=""
+                        <img class="img-fluid position-absolute w-100 h-100" src="content/img/golden-book-sports-pwc.png" alt=""
                             style="object-fit: cover;">
                     </div>
                 </div>
