d6afdfd9ed10595cfcddf9fcb66b5c90e898bafb
Nelitha Vindinu Priyawansha <nelithavindinu7@gmail.com>
2023-09-05 09:09:05 +0530

    analytics install

--- events/header.php
+++ events/header.php
@@ -93,6 +93,16 @@
         }
     </style>
 
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-K1KCZVJTWP"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-K1KCZVJTWP');
+</script>
+
 </head>
 
 <body>
