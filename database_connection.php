f2e8439f5fb2c3fca4824f8df0e70aeddb0b89ab
Nelitha Vindinu Priyawansha <nelithavindinu7@gmail.com>
2023-09-28 10:11:07 +0530

    Update database_connection.php

--- database_connection.php
+++ database_connection.php
@@ -2,6 +2,8 @@
 
 //database_connection.php
 
+session_start();
+
 $connect = new PDO("mysql:host=localhost; dbname=pwc_db", "root", "");
 
 // $connect = new PDO("mysql:host=localhost:3306; dbname=pwc_db", "admin_pwc", "s63pv7I@1");
