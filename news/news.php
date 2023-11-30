4ba776cb968a746b6229f93c9ddfd10b17c6706b
nelitha13 <nelithavindinu7@gmail.com>
2023-11-30 17:08:42 +0530

    schl pride annimation

--- news/news.php
+++ news/news.php
@@ -49,6 +49,49 @@ foreach ($rows as $row) {
 
 <body>
 
+<?php
+// Assuming you have fetched the category field value from the database into $row["category"]
+$category = $row["category"];
+
+if (stripos($category, "Achievement") !== false) {
+
+    echo '<div class="confetti-container"></div>';
+    
+    // Include the confetti script
+    echo '<script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.9.2/dist/confetti.browser.min.js"></script>';
+    
+    // Run the confetti animation script
+    echo '
+    <script>
+      var end = Date.now() + (3 * 1000);
+      var colors = [\'#800080\', \'#ffd700\', \'#800000\'];
+    
+      (function frame() {
+        confetti({
+          particleCount: 3,
+          angle: 60,
+          spread: 55,
+          origin: { x: 0 },
+          colors: colors
+        });
+        confetti({
+          particleCount: 3,
+          angle: 120,
+          spread: 55,
+          origin: { x: 1 },
+          colors: colors
+        });
+    
+        if (Date.now() < end) {
+          requestAnimationFrame(frame);
+        }
+      }());
+    </script>';
+}
+?>
+
+
+
   <header>
     <br><br><br>
 
